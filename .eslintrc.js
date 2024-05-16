c3e61534ca7e42e9e5008494c46588f11fa22d21
bob0kaaa <alimovdf@gmail.com>
2024-05-16 10:23:44 +0500

    edit eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
+        // eslint-disable-next-line max-len
         'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
