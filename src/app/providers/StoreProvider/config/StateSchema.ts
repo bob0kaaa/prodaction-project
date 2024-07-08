591f114869d0f6670b345960817953146124cf14
bob0kaaa <alimovdf@gmail.com>
2024-07-08 16:37:53 +0500

    add fetching articles, view type selector

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -12,6 +12,7 @@ import { NavigateOptions } from 'react-router';
 import { ArticleDetailsSchema } from 'entities/Article';
 import { ArticleDetailsCommentsSchema } from 'pages/ArticleDetailsPage';
 import { AddCommentFormSchema } from 'features/AddCommentForm';
+import { ArticlePageSchema } from 'pages/ArticlesPage';
 
 export interface StateSchema {
     counter: CounterSchema;
@@ -23,6 +24,7 @@ export interface StateSchema {
     articleDetails?: ArticleDetailsSchema;
     articleDetailsComments?: ArticleDetailsCommentsSchema;
     addCommentForm?: AddCommentFormSchema;
+    articlesPage?: ArticlePageSchema;
 }
 
 export type StateSchemaKey = keyof StateSchema;
---
+++ src/pages/ArticlesPage/model/slices/articlesPageSlice.ts
@@ -0,0 +1,58 @@
+import { createEntityAdapter, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { Article, ArticleView } from 'entities/Article';
+import { ArticlePageSchema } from 'pages/ArticlesPage';
+import { ARTICLES_VIEW_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
+import { fetchArticleList } from '../../model/services/fetchArticlesList/fetchArticlesList';
+
+const articlesAdapter = createEntityAdapter<Article>({
+    selectId: (article) => article.id,
+});
+
+export const getArticles = articlesAdapter.getSelectors<StateSchema>(
+    (state) => state.articlesPage || articlesAdapter.getInitialState(),
+);
+
+// @ts-ignore
+const articlesPageSlice = createSlice({
+    name: 'articlesPageSlice',
+    initialState: articlesAdapter.getInitialState<ArticlePageSchema>({
+        entities: {},
+        error: undefined,
+        ids: [],
+        isLoading: false,
+        view: ArticleView.SMALL,
+    }),
+    reducers: {
+        setView: (state, action: PayloadAction<ArticleView>) => {
+            state.view = action.payload;
+            localStorage.setItem(ARTICLES_VIEW_LOCALSTORAGE_KEY, action.payload);
+        },
+        initState: (state) => {
+            state.view = localStorage.getItem(ARTICLES_VIEW_LOCALSTORAGE_KEY) as ArticleView;
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchArticleList.pending, (state) => {
+                state.error = undefined;
+                state.isLoading = true;
+            })
+            .addCase(fetchArticleList.fulfilled, (
+                state,
+                action: PayloadAction<Article[]>,
+            ) => {
+                state.isLoading = false;
+                articlesAdapter.setAll(state, action.payload);
+            })
+            .addCase(fetchArticleList.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            });
+    },
+});
+
+export const {
+    reducer: articlesPageReducer,
+    actions: articlesPageActions,
+} = articlesPageSlice;
