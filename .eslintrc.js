7230381ab9622b99d2d96c34c4238afe0bd30e2d
bob0kaaa <alimovdf@gmail.com>
2024-07-30 15:42:42 +0500

    edit param lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,7 +38,7 @@ module.exports = {
             'error',
             {
                 markupOnly: true,
-                ignoreAttribute: ['data-testid', 'to'],
+                ignoreAttribute: ['data-testid', 'to', 'target'],
             },
         ],
         'max-len': ['error', {
